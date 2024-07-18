2ac1df1a8bd107bd2e98d1729aa69566448297da
tikhon <tikhon.radk@gmail.com>
2024-07-18 16:18:59 +0300

    new pie plot func created

--- chesswinnerprediction/visualization/visualization.py
+++ chesswinnerprediction/visualization/visualization.py
@@ -1,7 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+def plot_pie(ax, data, column_name, title, threshold=0):
+    value_counts = data[column_name].value_counts()
+    mask = value_counts.values / len(data) < threshold
+    other_count = value_counts[mask].sum()
+    value_counts = value_counts[~mask]
+
+    if other_count > 0:
+        value_counts["Other"] = other_count
+
+    ax.pie(value_counts, labels=value_counts.index, autopct="%1.1f%%", startangle=140)
+    ax.set_title(title)
 
 
+
+
+
+# TODO: remove
 # def make_pie_plot(data: pd.DataFrame, column_name):
 #     value_counts = data[column_name].value_counts()
 #     plt.figure(figsize=(6, 6))
@@ -25,14 +38,3 @@ import matplotlib.pyplot as plt
 #     plt.title(title)
 #     plt.show()
 
-def plot_pie(ax, data, column_name, title, threshold=0):
-    value_counts = data[column_name].value_counts()
-    mask = value_counts.values / len(data) < threshold
-    other_count = value_counts[mask].sum()
-    value_counts = value_counts[~mask]
-
-    if other_count > 0:
-        value_counts["Other"] = other_count
-
-    ax.pie(value_counts, labels=value_counts.index, autopct="%1.1f%%", startangle=140)
-    ax.set_title(title)
