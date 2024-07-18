c6bb91a97020ba139b8d8bb984d8fe98e415452f
tikhon <tikhon.radk@gmail.com>
2024-07-18 16:30:23 +0300

    added comments to MIN_DATE

--- chesswinnerprediction/config.py
+++ chesswinnerprediction/config.py
@@ -1,4 +1,4 @@
-MIN_DATE = "2017-03"
+MIN_DATE = "2017-03"  # 2017 - March; size - 2.17 GB; games - 11,346,745
 EXAMPLE_URL = "https://database.lichess.org/standard/lichess_db_standard_rated_2017-03.pgn.zst"
 EXTERNAL_FOLDER_PATH = "../../data/external"
 RAW_FOLDER_PATH = "../../data/raw"
