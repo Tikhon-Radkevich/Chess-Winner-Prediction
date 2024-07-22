a819545cb8e3164162693217792ad86ff29bcd5d
tikhon <tikhon.radk@gmail.com>
2024-07-22 20:09:36 +0300

    refactoring; fix data dirs path

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,4 +1,11 @@
+import os
+
+from config import ROOT_DIR
+
+
 MIN_DATE = "2017-03"  # 2017 - March; size - 2.17 GB; games - 11,346,745
 EXAMPLE_URL = "https://database.lichess.org/standard/lichess_db_standard_rated_2017-03.pgn.zst"
-EXTERNAL_FOLDER_PATH = "../../../data/external"
-RAW_FOLDER_PATH = "../../../data/raw"
+
+EXTERNAL_FOLDER_PATH = os.path.join(ROOT_DIR, "data", "external")
+RAW_FOLDER_PATH = os.path.join(ROOT_DIR, "data", "raw")
+
--- scripts/download_as_csv.py
+++ scripts/download_as_csv.py
@@ -7,28 +7,28 @@ from constants import (
     EXTERNAL_FOLDER_PATH,
     RAW_FOLDER_PATH,
 )
-from download_pgn_zst import download_file
-from pgn_zst_to_csv import pgn_zst_to_dataframe
+from chesswinnerprediction import download_pgn_zst_file
+from chesswinnerprediction import pgn_zst_to_dataframe
 
 
 def main(url):
     date = url.split("_")[-1].replace(".pgn.zst", "")
     if date < MIN_DATE:
         raise ValueError(
-            f"Data in {date} does not contain 'clk' and 'eval' data. You must download data from {MIN_DATE} or later!"
+            f"Data in {date} does not contain 'clk' and 'eval' data. You must dataloader data from {MIN_DATE} or later!"
         )
 
-    file_path = download_file(url, EXTERNAL_FOLDER_PATH)
+    file_path = download_pgn_zst_file(url, EXTERNAL_FOLDER_PATH)
     file_name = os.path.basename(file_path)
 
     csv_files_dir_name = file_name.replace(".pgn.zst", "")
     csv_files_dir = os.path.join(RAW_FOLDER_PATH, str(csv_files_dir_name))
     if os.path.exists(csv_files_dir):
-        raise ValueError(f"To process {file_name} again, delete the directory: {os.path.abspath(csv_files_dir)}")
+        raise ValueError(f"To process {file_name} again, delete the directory: {csv_files_dir}")
 
     os.makedirs(csv_files_dir)
 
-    pgn_zst_to_dataframe(file_path, os.path.abspath(csv_files_dir))
+    pgn_zst_to_dataframe(file_path, csv_files_dir)
 
 
 if __name__ == "__main__":
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         "url",
         nargs="?",
         default=EXAMPLE_URL,
-        help=f"URL from https://database.lichess.org/ of the .pgn.zst file to download. Default: {EXAMPLE_URL}",
+        help=f"URL from https://database.lichess.org/ of the .pgn.zst file to dataloader. Default: {EXAMPLE_URL}",
     )
     args = parser.parse_args()
 
