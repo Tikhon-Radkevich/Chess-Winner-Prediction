6b12436edeefe9f907070f94ea5e8f806b3c7285
tikhon <tikhon.radk@gmail.com>
2024-07-24 14:05:07 +0300

    plot_draw_percentage_by_base_time method added

--- chesswinnerprediction/visualization/visualization.py
+++ chesswinnerprediction/visualization/visualization.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+
 def plot_pie(ax, data, column_name, title, threshold=0):
     value_counts = data[column_name].value_counts()
     mask = value_counts.values / len(data) < threshold
@@ -10,6 +13,32 @@ def plot_pie(ax, data, column_name, title, threshold=0):
     ax.pie(value_counts, labels=value_counts.index, autopct="%1.1f%%", startangle=140)
     ax.set_title(title)
 
+
+def plot_draw_percentage_by_base_time(data_df, base_times=None):
+    """
+    Plots the draw percentage by base time.
+
+    :param data_df: DataFrame with "BaseTime" and "Draw" columns.
+    :param base_times: Optional; either a list of specific base times or an integer n for the top n popular base times.
+    """
+    if base_times is None:
+        base_time_df = data_df[["BaseTime", "Draw"]]
+    elif isinstance(base_times, int):
+        base_time_counts = data_df["BaseTime"].value_counts()
+        top_base_times = base_time_counts.head(base_times).index
+        base_time_df = data_df[data_df["BaseTime"].isin(top_base_times)][["BaseTime", "Draw"]]
+    else:
+        base_time_df = data_df[data_df["BaseTime"].isin(base_times)][["BaseTime", "Draw"]]
+
+    pct_draw_values = base_time_df.groupby("BaseTime").mean() * 100
+
+    _, plot_ax = plt.subplots(figsize=(12, 6))
+    pct_draw_values.plot(kind="bar", ax=plot_ax)
+    plot_ax.set_title("Draw Percentage by Base Time")
+    plot_ax.set_ylabel("Draw Percentage (%)")
+    plot_ax.set_xlabel("Base Time")
+    plt.xticks(rotation=45)
+    plt.show()
 
 
 
