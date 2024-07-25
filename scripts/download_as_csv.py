3cd81df6383b9933ef016f2666fc7c32c48c80ed
tikhon <tikhon.radk@gmail.com>
2024-07-25 23:05:57 +0300

    split_size arg added to argparser

--- scripts/download_as_csv.py
+++ scripts/download_as_csv.py
@@ -7,15 +7,14 @@ from constants import (
     EXTERNAL_FOLDER_PATH,
     RAW_FOLDER_PATH,
 )
-from chesswinnerprediction import download_pgn_zst_file
-from chesswinnerprediction import pgn_zst_to_dataframe
+from chesswinnerprediction import download_pgn_zst_file, pgn_zst_to_dataframe
 
 
-def main(url):
+def main(url, split_size):
     date = url.split("_")[-1].replace(".pgn.zst", "")
     if date < MIN_DATE:
         raise ValueError(
-            f"Data in {date} does not contain 'clk' and 'eval' data. You must dataloader data from {MIN_DATE} or later!"
+            f"Data in {date} does not contain 'clk' and 'eval' data. You must use data from {MIN_DATE} or later!"
         )
 
     file_path = download_pgn_zst_file(url, EXTERNAL_FOLDER_PATH)
@@ -28,7 +27,7 @@ def main(url):
 
     os.makedirs(csv_files_dir)
 
-    pgn_zst_to_dataframe(file_path, csv_files_dir)
+    pgn_zst_to_dataframe(file_path, csv_files_dir, split_size=split_size)
 
 
 if __name__ == "__main__":
@@ -37,12 +36,18 @@ if __name__ == "__main__":
         "url",
         nargs="?",
         default=EXAMPLE_URL,
-        help=f"URL from https://database.lichess.org/ of the .pgn.zst file to dataloader. Default: {EXAMPLE_URL}",
+        help=f"URL from https://database.lichess.org/ of the .pgn.zst file to download. Default: {EXAMPLE_URL}",
+    )
+    parser.add_argument(
+        "--split_size",
+        type=int,
+        default=125000,
+        help="Size to split the PGN files into. Default: 125000",
     )
     args = parser.parse_args()
 
     try:
-        main(args.url)
+        main(args.url, args.split_size)
     except Exception as e:
         print(f"Error: {e}")
         exit(1)
