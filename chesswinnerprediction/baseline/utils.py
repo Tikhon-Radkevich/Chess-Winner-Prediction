229b8a4a9a6d5ac0d9d4a4c135393c368d20133e
tikhon <tikhon.radk@gmail.com>
2024-08-01 12:58:06 +0300

    fix feature importance plot

--- chesswinnerprediction/baseline/utils.py
+++ chesswinnerprediction/baseline/utils.py
@@ -1,33 +1,72 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import ConfusionMatrixDisplay, classification_report, confusion_matrix, log_loss
 
 from chesswinnerprediction.constants import RESULTS_STR_TO_STR, DRAW_STR
+from chesswinnerprediction.baseline.constants import BASELINE_COLUMNS, columns_to_scale
 
 
 def show_feature_importance(model, feature_importance):
-    importance_df = pd.DataFrame({"Feature": model.feature_names_in_, "Importance": feature_importance})
-    importance_df.sort_values(by="Importance", ascending=False, inplace=True)
-    plt.barh(importance_df["Feature"], importance_df["Importance"], color="skyblue")
-    plt.xlabel("Importance")
-    plt.ylabel("Feature")
-    plt.title("Feature Importance")
-    plt.gca().invert_yaxis()
+    labels = [RESULTS_STR_TO_STR[label] for label in model.classes_]
+    num_classes = len(feature_importance)
+    importance_dfs = []
+    for i in range(num_classes):
+        importance_df = pd.DataFrame({
+            "Feature": model.feature_names_in_,
+            "Importance": feature_importance[i]
+        })
+        importance_df.sort_values(by="Importance", ascending=False, inplace=True)
+        importance_dfs.append(importance_df)
+
+    if num_classes == 1:
+        fig_size = (7, 5)
+    else:
+        fig_size = (15, 5)
+    _, axes = plt.subplots(ncols=num_classes, figsize=fig_size, sharey=True)
+    if num_classes == 1:
+        axes = [axes]
+
+    for i in range(num_classes):
+        sns.barplot(x="Importance", y="Feature", hue="Feature", data=importance_dfs[i], palette="viridis", ax=axes[i])
+
+        axes[i].set_xlabel("Importance", fontsize=12)
+        axes[i].set_title(labels[i], fontsize=14)
+        axes[i].invert_yaxis()
+
+    axes[0].set_ylabel("Feature", fontsize=12)  # Set ylabel only for the first subplot
+
+    plt.tight_layout()
     plt.show()
 
 
-def estimate_baseline_model(model, x_test, y_test, feature_importance):
+def print_report(model, x1, y1, x2=None, y2=None, report_title_1="Train Report", report_title_2="Validation Report"):
+    predict_1 = model.predict(x1)
+    report_1 = classification_report(y1, predict_1, zero_division=np.nan)
+    if x2 is None:
+        print("\n" + "\t" * 6 + f"Classification Report \n{report_1}")
+        return
+
+    predict_2 = model.predict(x2)
+    report_2 = classification_report(y2, predict_2, zero_division=np.nan)
+
+    print("\t" * 6, report_title_1, "\t" * 9, report_title_2)
+    for part_1, part_2 in zip(report_1.split("\n\n"), report_2.split("\n\n")):
+        for val_1, val_2 in zip(part_1.split("\n"), part_2.split("\n")):
+            print(val_1, " " * 6 + val_2[12:])
+
+
+def estimate_baseline_model(model, feature_importance, x_train, y_train, x_test, y_test):
     predict = model.predict(x_test)
     prob_predict = model.predict_proba(x_test)
 
     loss = log_loss(y_test, prob_predict)
-    print(f"log loss on test data: {loss}")
+    print(f"log loss on test data: {loss}\n")
 
-    report = classification_report(y_test, predict)
-    print("\n" + "\t"*6 + f"Classification Report \n{report}")
+    print_report(model, x_train, y_train, x_test, y_test)
 
     conf_matrix = confusion_matrix(y_test, predict, labels=model.classes_)
     labels = [RESULTS_STR_TO_STR[label] for label in model.classes_]
@@ -62,15 +101,11 @@ def get_x_and_y(data, predict_draws=False):
     return x_data, y_data
 
 
-def print_combined_report(model, x1, x2, y1, y2, report_title_1="Train Report", report_title_2="Validation Report"):
-    predict_1 = model.predict(x1)
-    predict_2 = model.predict(x2)
-
-    report_1 = classification_report(y1, predict_1, zero_division=np.nan)
-    report_2 = classification_report(y2, predict_2, zero_division=np.nan)
-
-    print("\t" * 6, report_title_1, "\t" * 9, report_title_2)
-    for part_1, part_2 in zip(report_1.split("\n\n"), report_2.split("\n\n")):
-        for val_1, val_2 in zip(part_1.split("\n"), part_2.split("\n")):
-            print(val_1, " " * 6 + val_2[12:])
-
+def transform_and_scale_df(df, scaler, fit_scale=True):
+    X = df[BASELINE_COLUMNS].copy()
+    X = pd.get_dummies(X, columns=["Event"], dtype=np.int8, prefix="", prefix_sep="")
+    if fit_scale:
+        X[columns_to_scale] = scaler.fit_transform(X[columns_to_scale])
+    else:
+        X[columns_to_scale] = scaler.transform(X[columns_to_scale])
+    return X
