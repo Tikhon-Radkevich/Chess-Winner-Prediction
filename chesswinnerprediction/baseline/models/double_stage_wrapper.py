b26207934c29bc0ae29b7e3df8268bc1780c297e
tikhon <tikhon.radk@gmail.com>
2024-08-17 19:05:51 +0300

    supported model added; refactoring

--- chesswinnerprediction/baseline/models/double_stage_wrapper.py
+++ chesswinnerprediction/baseline/models/double_stage_wrapper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -6,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.utils import compute_sample_weight
 from sklearn import metrics
 
-from chesswinnerprediction.baseline.custom_knn import CustomKNNClassifier
+from chesswinnerprediction.baseline.models.custom_knn import CustomKNNClassifier
 
 from chesswinnerprediction.constants import DRAW_STR
 from chesswinnerprediction.baseline.utils import estimate_baseline_model
@@ -16,27 +17,48 @@ class Model:
     win_to_draw_splitter_draw_symbol = False
     win_to_draw_splitter_win_symbol = True
 
-    def __init__(self, win_to_draw_splitter, black_to_white_splitter, name):
+    AVAILABLE_SPLITTERS = (
+        DecisionTreeClassifier
+        | RandomForestClassifier
+        | LogisticRegression
+        | GradientBoostingClassifier
+        | CustomKNNClassifier
+    )
+
+    def __init__(
+        self,
+        win_to_draw_splitter: AVAILABLE_SPLITTERS,
+        black_to_white_splitter: AVAILABLE_SPLITTERS,
+        name: str,
+    ) -> None:
         self.win_to_draw_splitter = win_to_draw_splitter
         self.black_to_white_splitter = black_to_white_splitter
         self.name = name
         self.classes_ = None
 
-    def fit(self, x, y):
+    def fit(self, x: pd.DataFrame, y: pd.Series) -> None:
         # Predict whether the game is a win or a draw
         win_condition = y != DRAW_STR
 
-        win_to_draw_splitter_sample_weight = compute_sample_weight("balanced", win_condition)
-        self.win_to_draw_splitter.fit(x, win_condition, win_to_draw_splitter_sample_weight)
+        win_to_draw_splitter_sample_weight = compute_sample_weight(
+            "balanced", win_condition
+        )
+        self.win_to_draw_splitter.fit(
+            x, win_condition, win_to_draw_splitter_sample_weight
+        )
 
         # If the game is not a draw, predict which player wins
-        black_to_white_sample_weight = compute_sample_weight("balanced", y[win_condition])
-        self.black_to_white_splitter.fit(x[win_condition], y[win_condition], black_to_white_sample_weight)
+        black_to_white_sample_weight = compute_sample_weight(
+            "balanced", y[win_condition]
+        )
+        self.black_to_white_splitter.fit(
+            x[win_condition], y[win_condition], black_to_white_sample_weight
+        )
 
         # Set class labels
         self.classes_ = [DRAW_STR, *self.black_to_white_splitter.classes_]
 
-    def predict_proba(self, x):
+    def predict_proba(self, x: pd.DataFrame) -> np.ndarray:
         win_probs = self.win_to_draw_splitter.predict_proba(x)
 
         classes = self.win_to_draw_splitter.classes_.tolist()
@@ -55,11 +77,11 @@ class Model:
 
         return probs
 
-    def score(self, x, y):
+    def score(self, x: pd.DataFrame, y: pd.Series) -> float:
         predictions = self.predict(x)
         return metrics.balanced_accuracy_score(y, predictions)
 
-    def predict(self, x):
+    def predict(self, x: pd.DataFrame) -> np.ndarray:
         is_win = self.win_to_draw_splitter.predict(x)
 
         predictions = np.full(x.shape[0], DRAW_STR, dtype=object)
@@ -69,20 +91,28 @@ class Model:
         return predictions
 
     @staticmethod
-    def _get_feature_importance(model):
+    def _get_feature_importance(model: AVAILABLE_SPLITTERS):
         if isinstance(model, (LogisticRegression, CustomKNNClassifier)):
             return model.coef_
-        # elif isinstance(model, DecisionTreeClassifier):
-        #     return [model.feature_importances_]
-        elif isinstance(model, (RandomForestClassifier, GradientBoostingClassifier, DecisionTreeClassifier)):
+        elif isinstance(
+            model,
+            (
+                RandomForestClassifier,
+                GradientBoostingClassifier,
+                DecisionTreeClassifier,
+            ),
+        ):
             return model.feature_importances_.reshape(1, -1)
 
         return None
 
-    def estimate_model(self, x_train, y_train_data, x_valid, y_valid_data, **kwargs):
-
+    def estimate_model(
+        self, x_train, y_train_data, x_valid, y_valid_data, **kwargs
+    ) -> None:
         feature_importance_1 = self._get_feature_importance(self.win_to_draw_splitter)
-        feature_importance_2 = self._get_feature_importance(self.black_to_white_splitter)
+        feature_importance_2 = self._get_feature_importance(
+            self.black_to_white_splitter
+        )
 
         win_condition_train = y_train_data != DRAW_STR
         win_condition_valid = y_valid_data != DRAW_STR
@@ -100,7 +130,7 @@ class Model:
             win_condition_train,
             x_valid,
             win_condition_valid,
-            **kwargs
+            **kwargs,
         )
         print(" " * 4, f"Black to White Splitter \n{self.black_to_white_splitter}")
         estimate_baseline_model(
@@ -110,15 +140,9 @@ class Model:
             black_to_white_y_train,
             black_to_white_x_valid,
             black_to_white_y_valid,
-            **kwargs
+            **kwargs,
         )
         print(" " * 4, "Overall:")
         estimate_baseline_model(
-            self,
-            None,
-            x_train,
-            y_train_data,
-            x_valid,
-            y_valid_data,
-            **kwargs
+            self, None, x_train, y_train_data, x_valid, y_valid_data, **kwargs
         )
