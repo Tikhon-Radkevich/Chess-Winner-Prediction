eb8add9fc8618e1ec7c8305693517bdd74d091a6
tikhon <tikhon.radk@gmail.com>
2024-08-18 18:19:57 +0300

    update log funcs: images moved to 'set_name' dir in artifacts

--- chesswinnerprediction/static_move/utils.py
+++ chesswinnerprediction/static_move/utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import mlflow
 import pandas as pd
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import recall_score, precision_score, balanced_accuracy_score
 
 from sklearn.preprocessing import StandardScaler
+from sklearn.utils.class_weight import compute_sample_weight
 
 from config import MLRUNS_FOLDER_PATH
 from chesswinnerprediction.constants import STATIC_MOVE_DATA_PATH
@@ -20,22 +22,35 @@ def get_x_and_y(data):
     return x_data, y_data
 
 
+def get_i_move_to_result_bins(data, n_bins):
+    bin_labels = [f"{i + 1}_bin" for i in range(n_bins)]
+    i_move_bin = pd.cut(data["i_move"], bins=n_bins, labels=bin_labels, include_lowest=True).astype(str)
+    i_move_bin_to_result = i_move_bin + "_" + data["Result"]
+    return i_move_bin_to_result
+
+
 def load_train_valid_test(
     # data_dir="lichess_db_standard_rated_2017-03",
-    train_sample_size=0.03,
-    valid_sample_size=0.05,
-    test_sample_size=1,
     random_state=RANDOM_STATE,
+    drop_event=True,
 ):
     # data_path = os.path.join(STATIC_MOVE_DATA_PATH)
+    n_bins = 24
 
     train_df = pd.read_csv(os.path.join(STATIC_MOVE_DATA_PATH, "train.csv"))
     valid_df = pd.read_csv(os.path.join(STATIC_MOVE_DATA_PATH, "valid.csv"))
     test_df = pd.read_csv(os.path.join(STATIC_MOVE_DATA_PATH, "test.csv"))
 
-    train_df.drop(columns=["Event"], inplace=True)
-    valid_df.drop(columns=["Event"], inplace=True)
-    test_df.drop(columns=["Event"], inplace=True)
+    if drop_event:
+        train_df.drop(columns=["Event"], inplace=True)
+        valid_df.drop(columns=["Event"], inplace=True)
+        test_df.drop(columns=["Event"], inplace=True)
+
+    i_move_bin_to_result = get_i_move_to_result_bins(train_df, n_bins)
+    train_df["sample_weight"] = compute_sample_weight("balanced", i_move_bin_to_result)
+
+    # valid_df = add_i_move_to_result_bins(valid_df, n_bins)
+    # test_df = add_i_move_to_result_bins(test_df, n_bins)
 
     # std_scaler = StandardScaler()
     # train_data = transform_and_scale_df(train_df, std_scaler)
@@ -72,15 +87,15 @@ def calculate_metrics(model, x, y_true, n_bins, i_move_bins):
 
         for class_label in recall_per_bin.keys():
             recall_per_bin[class_label][bin_id] = recall_score(
-                y_test_filtered, y_pred, labels=[class_label], average="macro"
+                y_test_filtered, y_pred, labels=[class_label], average=None
             )
             precision_per_bin[class_label][bin_id] = precision_score(
-                y_test_filtered, y_pred, labels=[class_label], average="macro"
+                y_test_filtered, y_pred, labels=[class_label], average=None
             )
     return balanced_accuracy_per_bin, recall_per_bin, precision_per_bin
 
 
-def log_metrics_plot(metrics_per_bin, bin_centers, n_bins, title):
+def save_metrics_plot(metrics_per_bin, bin_centers, n_bins, dir_path, title):
     plt.figure(figsize=(12, 6))
 
     for class_label in metrics_per_bin.keys():
@@ -93,16 +108,12 @@ def log_metrics_plot(metrics_per_bin, bin_centers, n_bins, title):
     plt.legend()
     plt.grid(True)
     # Save the plot as an image file
-    image_path = f"{title.lower().replace(' ', '_')}_plot.png"
+    image_path = os.path.join(dir_path, f"{title.lower().replace(' ', '_')}_plot.png")
     plt.savefig(image_path)
     plt.close()
 
-    mlflow.log_artifact(image_path)
 
-    os.remove(image_path)
-
-
-def log_x_distribution(n_bins, i_move_bins, y_true, bin_centers):
+def save_x_distribution(n_bins, i_move_bins, y_true, bin_centers, dir_path):
     class_distribution_per_bin = pd.DataFrame(0, index=range(n_bins), columns=["1-0", "0-1", "1/2-1/2"])
 
     for bin_id in range(n_bins):
@@ -117,29 +128,34 @@ def log_x_distribution(n_bins, i_move_bins, y_true, bin_centers):
     bottom_values = np.zeros(n_bins)
 
     for class_label in class_distribution_per_bin.columns:
-        plt.bar(bin_centers, class_distribution_per_bin[class_label], width=bar_width, bottom=bottom_values,
-                label=class_label)
+        plt.bar(bin_centers,
+                class_distribution_per_bin[class_label],
+                width=bar_width,
+                bottom=bottom_values,
+                label=class_label,
+                edgecolor='black'
+                )
         bottom_values += class_distribution_per_bin[class_label]
 
-    title = "Class Distribution vs. i_move (Binned)"
+    title = "Class Distribution vs i_move (Binned)"
     plt.xlabel("i_move (Step Number)")
     plt.ylabel("Count")
     plt.title(title)
     plt.legend(title="Class")
-    plt.grid(True)
+    # plt.grid(True)
 
-    image_path = f"{title.lower().replace(' ', '_')}_plot.png"
+    image_path = os.path.join(dir_path, f"{title.lower().replace(' ', '_')}_plot.png")
     plt.savefig(image_path)
     plt.close()
 
-    mlflow.log_artifact(image_path)
 
-    os.remove(image_path)
+def log_prediction(model, x, y_true, set_name):
+    if not os.path.exists(set_name):
+        os.mkdir(set_name)
+    balanced_accuracy = balanced_accuracy_score(y_true, model.predict(x))
+    mlflow.log_metric(f"{set_name} Balanced Accuracy", balanced_accuracy)
 
-
-def log_prediction(model, x, y_true):
     n_bins = 24
-
     bin_edges = np.linspace(0, max(x["i_move"]), n_bins + 1)
     i_move_bins = pd.cut(x["i_move"], bins=bin_edges, labels=False, include_lowest=True)
 
@@ -150,6 +166,9 @@ def log_prediction(model, x, y_true):
     recall["Accuracy"] = balanced_accuracy
     precision["Accuracy"] = balanced_accuracy
 
-    log_metrics_plot(recall, bin_centers, n_bins, "Recall")
-    log_metrics_plot(precision, bin_centers, n_bins, "Precision")
-    log_x_distribution(n_bins, i_move_bins, y_true, bin_centers)
+    save_metrics_plot(recall, bin_centers, n_bins, set_name, "Recall")
+    save_metrics_plot(precision, bin_centers, n_bins, set_name, "Precision")
+    save_x_distribution(n_bins, i_move_bins, y_true, bin_centers, set_name)
+
+    mlflow.log_artifact(set_name)
+    shutil.rmtree(set_name)
