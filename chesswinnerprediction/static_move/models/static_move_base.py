f1332786f11c65c611c10b75bcc6ef3b96b39174
tikhon <tikhon.radk@gmail.com>
2024-08-31 21:49:58 +0300

    sample weights added to score func for balanced accuracy

--- chesswinnerprediction/static_move/models/static_move_base.py
+++ chesswinnerprediction/static_move/models/static_move_base.py
@@ -10,17 +10,17 @@ class StaticMoveBaseModel:
         self.balanced_accuracy = None
         self.metric_name = "Balanced Accuracy"
 
-    def fit(self, x, y):
-        self._estimator.fit(x, y)
+    def fit(self, x, y, **kwargs):
+        self._estimator.fit(x, y, **kwargs)
 
     def predict(self, x):
         return self._estimator.predict(x)
 
     def log_trial(self):
-        mlflow.log_metric(self.metric_name, self.balanced_accuracy)
+        model_params = self._estimator.get_params()
+        mlflow.log_params(model_params)
+        mlflow.log_metric("X_valid " + self.metric_name, self.balanced_accuracy)
 
-    def score(self, x, y):
+    def score(self, x, y, sample_weight=None):
         y_predict = self.predict(x)
-        self.balanced_accuracy = metrics.balanced_accuracy_score(y, y_predict)
-
-
+        self.balanced_accuracy = metrics.balanced_accuracy_score(y, y_predict, sample_weight=sample_weight)
