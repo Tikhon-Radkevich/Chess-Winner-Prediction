901f32c70f761c772db20520aaf5f6d80aa8a3c6
tikhon <tikhon.radk@gmail.com>
2024-08-31 15:10:44 +0300

    sample_original.py converted to script

--- chesswinnerprediction/static_move/constants.py
+++ chesswinnerprediction/static_move/constants.py
@@ -1,25 +1,31 @@
 RANDOM_STATE = 42
 
+PIECE_VALUES = [0, 1, 3, 3, 5, 9, 0]
 
 STATIC_MOVE_COLUMNS = [
     "GameId",
     "Event",
-    "WhiteElo",
-    "BlackElo",
+    # "WhiteElo",
+    # "BlackElo",
     "Result",
     "eval",
     "EloDiff",
     "MeanElo",
     "BaseTime",
     "IncrementTime",
-    "ZeroIncrementTime",
-    "white_remaining_time",
-    "black_remaining_time",
-    "mean_base_time",
-    "time_diff",
+    # "ZeroIncrementTime",
+    # "white_remaining_time",
+    # "black_remaining_time",
+    # "mean_base_time",
+    "time_diff_norm",
     "white_remaining_time_norm",
     "black_remaining_time_norm",
-    "GameDurations",
+    "GameDurations_norm",
+    "chess_moves_list",
     "is_checkmate_countdown",
     "i_move",
 ]
+
+TRAIN_VALID_TEST = ("train.csv", "valid.csv", "test.csv")
+I_MOVE_THRESHOLD = (150, 150, 200)
+DEFAULT_N_SAMPLES = (60_000, 25_000, None)
--- scripts/static_move/sample_original.py
+++ scripts/static_move/sample_original.py
@@ -1,53 +1,56 @@
 import os
+import argparse
 
 import pandas as pd
-import numpy as np
 
+from chesswinnerprediction.static_move.processing.utils import process_df
 from chesswinnerprediction.constants import INTERIM_STATIC_MOVE, PROCESSED_STATIC_MOVE
+from chesswinnerprediction.static_move.constants import (
+    TRAIN_VALID_TEST,
+    I_MOVE_THRESHOLD,
+    DEFAULT_N_SAMPLES,
+)
 
 
-def process_df(df: pd.DataFrame, n, random_state, i_move_threshold):
-    valid_con = (df["i_move"] == 1) & (
-        df["black_remaining_time_norm"] == df["white_remaining_time_norm"]
-    )
-    df = df[df["GameId"].isin(df[valid_con]["GameId"])]
-
-    df = df[df["i_move"] < i_move_threshold]
-    df = df.replace([np.inf, -np.inf], np.nan)
-    df.dropna(inplace=True)
-    df.drop(columns=["GameId"], inplace=True)
-
-    if n is not None:
-        df = df.sample(n=n, random_state=random_state)
-
-    df[["black_remaining_time_norm", "white_remaining_time_norm"]] = df[
-        ["black_remaining_time_norm", "white_remaining_time_norm"]
-    ].clip(lower=0.0, upper=1.0)
-    df["GameDurations_norm"] = df["GameDurations_norm"].clip(lower=0.0, upper=2.0)
-    df["time_diff_norm"] = df["time_diff_norm"].clip(lower=-1.0, upper=1.0)
-    return df
-
-
-def main():
+def main(dir_name, train_valid_test_samples):
     random_state = 42
-    dir_name = "lichess_db_standard_rated_2017-05"
 
-    interim_dir_path = os.path.join(INTERIM_STATIC_MOVE, dir_name)
-    processed_dir_path = os.path.join(PROCESSED_STATIC_MOVE, dir_name)
+    interim_dir_path = str(os.path.join(INTERIM_STATIC_MOVE, dir_name))
+    processed_dir_path = str(os.path.join(PROCESSED_STATIC_MOVE, dir_name))
     if not os.path.exists(processed_dir_path):
         os.mkdir(processed_dir_path)
 
-    train_valid_test = ("train.csv", "valid.csv", "test.csv")
-    i_move_thresholds = (150, 150, 200)
-    train_valid_test_samples = (60_000, 20_000, None)
+    if train_valid_test_samples is None:
+        train_valid_test_samples = DEFAULT_N_SAMPLES
 
     for csv_file, n, i_move_threshold in zip(
-        train_valid_test, train_valid_test_samples, i_move_thresholds
+        TRAIN_VALID_TEST, train_valid_test_samples, I_MOVE_THRESHOLD
     ):
+        print(f"Processing {csv_file}...")
         df = pd.read_csv(os.path.join(interim_dir_path, csv_file))
         df = process_df(df, n, random_state, i_move_threshold)
         df.to_csv(os.path.join(processed_dir_path, csv_file), index=False)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Process chess PGN files into train/valid/test datasets."
+    )
+    parser.add_argument(
+        "dir_name",
+        help=f"Directory name from {INTERIM_STATIC_MOVE}, containing the chess data files.",
+    )
+    parser.add_argument(
+        "--train_valid_test_samples",
+        nargs=3,
+        type=int,
+        default=[60_000, 25_000, None],
+        help="Number of samples for train, valid, and test datasets. Use 'None' to include all remaining data in set.",
+    )
+    args = parser.parse_args()
+
+    try:
+        main(args.dir_name, args.train_valid_test_samples)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
