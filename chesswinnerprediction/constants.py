18f2d70446354f4a1d451a3d6299bd4759863cc1
tikhon <tikhon.radk@gmail.com>
2024-09-06 21:21:05 +0300

    refactoring; removed unused constants

--- chesswinnerprediction/constants.py
+++ chesswinnerprediction/constants.py
@@ -3,7 +3,8 @@ import os
 from config import ROOT_DIR
 
 
-# Dataset constants
+# Dataset
+# lichess standard games datasets: https://database.lichess.org/#standard_games
 MIN_DATE = "2017-03"  # 2017 - March; size - 2.17 GB; games - 11,346,745
 BASE_NAME = "lichess_db_standard_rated_"
 EXAMPLE_NAME = BASE_NAME + MIN_DATE
@@ -17,40 +18,28 @@ INTERIM_FOLDER_PATH = os.path.join(ROOT_DIR, "data", "interim")
 
 EXAMPLE_CSV_DIR = os.path.join(RAW_FOLDER_PATH, EXAMPLE_NAME)
 
+# Demo Models
 MODELS_DIR = os.path.join(ROOT_DIR, "models")
 BASELINE_MODEL = os.path.join(MODELS_DIR, "baseline.pkl")
 STATIC_MOVE_MODEL = os.path.join(MODELS_DIR, "static_move.pkl")
 
+# Demo Data
 DEMO_DATA_DIR = os.path.join(PROCESSED_FOLDER_PATH, "demo")
 BASELINE_DEMO_DATA = os.path.join(DEMO_DATA_DIR, "baseline.csv")
 STATIC_MOVE_DEMO_DATA = os.path.join(DEMO_DATA_DIR, "static_move.csv")
 INTERIM_DEMO_DATA = os.path.join(DEMO_DATA_DIR, "interim.csv")
 
-# static_move constants
-# todo: remove commented code
+# static_move Constants
 STATIC_MOVE = "static_move"
 INTERIM_STATIC_MOVE = os.path.join(INTERIM_FOLDER_PATH, STATIC_MOVE)
-# INTERIM_STATIC_MOVE_SPLIT_BALANCED = os.path.join(INTERIM_STATIC_MOVE_SPLIT, "balanced")
-# INTERIM_STATIC_MOVE_SPLIT_ORIGINAL = os.path.join(INTERIM_STATIC_MOVE_SPLIT, "original")
-# INTERIM_STATIC_MOVE_SPLIT_EXTRA = os.path.join(INTERIM_STATIC_MOVE_SPLIT, "extra")
-
 PROCESSED_STATIC_MOVE = os.path.join(PROCESSED_FOLDER_PATH, STATIC_MOVE)
-# PROCESSED_STATIC_MOVE_ORIGINAL = os.path.join(PROCESSED_FOLDER_PATH, STATIC_MOVE, "original")
-# STATIC_MOVE_DATA_PATH = os.path.join(ROOT_DIR, "data", "processed", "static_move")
-GAME_ID = "GameId"
 
 if not os.path.exists(INTERIM_STATIC_MOVE):
     os.makedirs(INTERIM_STATIC_MOVE)
-# if not os.path.exists(INTERIM_STATIC_MOVE_SPLIT_BALANCED):
-#     os.makedirs(INTERIM_STATIC_MOVE_SPLIT_BALANCED)
-# if not os.path.exists(INTERIM_STATIC_MOVE_SPLIT_ORIGINAL):
-#     os.makedirs(INTERIM_STATIC_MOVE_SPLIT_ORIGINAL)
-# if not os.path.exists(INTERIM_STATIC_MOVE_SPLIT_EXTRA):
-#     os.makedirs(INTERIM_STATIC_MOVE_SPLIT_EXTRA)
 if not os.path.exists(PROCESSED_STATIC_MOVE):
     os.makedirs(PROCESSED_STATIC_MOVE)
 
-# Results constants
+# Results Constants
 WHITE_WIN_STR: str = "1-0"
 BLACK_WIN_STR: str = "0-1"
 DRAW_STR: str = "1/2-1/2"
@@ -78,7 +67,7 @@ RESULTS_STR_TO_STR = {
     True: "Win",
 }
 
-# Data processing constants
+# Data Processing Constants
 MIN_GAME_DURATION = 0  # seconds
 MIN_MOVES_IN_GAME = 4
 
@@ -97,5 +86,3 @@ PROCESS_COLUMNS = [
     "Black",
     "parse_success",
 ]
-
-# TARGET_COLUMNS = ["ResultEncoded", "WhiteWin", "BlackWin", "Draw"]
